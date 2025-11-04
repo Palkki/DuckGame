8295d9421028750c24ab1a07005b2a7a84c44af2
Palkki <jasu.kansala@gmail.com>
2025-11-04 20:57:00 +0200

    more thingies

--- game.py
+++ game.py
@@ -7,8 +7,8 @@ WIN_HEIGHT = 600
 GRAVITY = 0.1
 
 game = {
-    "x": 40,
-    "y": 40,
+    "x": 100,
+    "y": 120,
     "angle": 0,
     "force": 0,
     "x_velocity": 0,
@@ -18,27 +18,78 @@ game = {
     "boxes": []
 }
 
+# region Duck
+    def initial_state():
+        """
+        Puts the game back into its initial state: the duck is put back into the
+        launch position, its speed to zero, and its flight state to False.
+        """
+        game["x"] = 40
+        game["y"] = 40
+        game["x_velocity"] = 0
+        game["y_velocity"] = 0
+        game["flight"] = False
 
-def initial_state():
+
+    def launch():
+        """
+        Launches a duck and calculates its starting velocity. Stores x and y velocity
+        components to the game dictionary.
+        """
+        game["x_velocity"] = game["force"] * math.cos(math.radians(game["angle"]))
+        game["y_velocity"] = game["force"] * math.sin(math.radians(game["angle"]))
+        game["flight"] = True
+
+def flight(elapsed):
     """
-    Puts the game back into its initial state: the duck is put back into the
-    launch position, its speed to zero, and its flight state to False.
+    Updates duck's x and y coordinates based on corresponding velocity vectors.
     """
-    game["x"] = 40
-    game["y"] = 40
-    game["x_velocity"] = 0
-    game["y_velocity"] = 0
-    game["flight"] = False
+    if game["flight"]:
+        game["x"] += game["x_velocity"]
+        game["y"] += game["y_velocity"]
+        game["y_velocity"] -= GRAVITATIONAL_ACCEL
+        if game["y"] <= 0:
+            initial_state()
+
+# endregion
+
+# region Inputs
+    def keypress(sym, mods):
+        """
+        This function handles keyboard input.
+        You do NOT need to modify this.
+        """
+        key = sweeperlib.pyglet.window.key
+
+        if sym == key.Q:
+            sweeperlib.close()
+
+        if sym == key.R:
+            initial_state()
+
+        if sym == key.RIGHT:
+            game["angle"] -= 10
+            if game["angle"] < 0:
+                game["angle"] = 350
+        elif sym == key.LEFT:
+            game["angle"] += 10
+            if game["angle"] > 350:
+                game["angle"] = 0
+
+        if sym == key.UP:
+            if game["force"] < 50:
+                game["force"] += 5
+        elif sym == key.DOWN:
+            if game["force"] >= 5:
+                game["force"] -= 5
+            else:
+                game["force"] = 0
+
+        if sym == key.SPACE:
+            launch()
 
 
-def launch():
-    """
-    Launches a duck and calculates its starting velocity. Stores x and y velocity
-    components to the game dictionary.
-    """
-    game["x_velocity"] = game["force"] * math.cos(math.radians(game["angle"]))
-    game["y_velocity"] = game["force"] * math.sin(math.radians(game["angle"]))
-    game["flight"] = True
+# endregion
 
 
 def draw():
@@ -48,7 +99,8 @@ def draw():
     sweeperlib.clear_window()
     sweeperlib.draw_background()
     sweeperlib.begin_sprite_draw()
-    sweeperlib.prepare_sprite("sling", game["x"], game["y"])
+    sweeperlib.prepare_sprite("sling", 80, 20)
+    sweeperlib.prepare_sprite("duck", game["x"], game["y"])
     sweeperlib.draw_sprites()
     sweeperlib.draw_text("{}°\tforce: {}".format(game["angle"], game["force"]), 10, 505)
     sweeperlib.draw_text(
@@ -61,10 +113,14 @@ def draw():
         size=20)
 
 
+
+
 def main():
     sweeperlib.load_sprites("sprites")
     sweeperlib.create_window(width=WIN_WIDTH, height=WIN_HEIGHT)
     sweeperlib.set_draw_handler(draw)
+    sweeperlib.set_keyboard_handler(keypress)
+    sweeperlib.set_interval_handler(flight)
     sweeperlib.start()
 
 
